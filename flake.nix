2609608850e7f460220e52ea3b31ddbcdec8654f
Squamto <25984337+Squamto@users.noreply.github.com>
2024-04-04 13:18:45 +0200

    init + simple zig

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: {
+    templates.simple-zig = {
+      path = ./simple-zig;
+      description = "A simple zig program";
+    };
+  };
+}
